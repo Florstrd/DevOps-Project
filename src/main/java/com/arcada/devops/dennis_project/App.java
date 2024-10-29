8bebc21369b1db28eda5d5c0fdd9ea140fa08e26
DennisFlorstrom <dennisflorstrom@gmail.com>
2024-10-29 16:54:40 +0200

    v1

--- src/main/java/com/arcada/devops/dennis_project/App.java
+++ src/main/java/com/arcada/devops/dennis_project/App.java
@@ -1,7 +1,34 @@
 package com.arcada.devops.dennis_project;
 
+import java.util.Scanner;
+
 public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+    public static void main(String[] args) {
+    	Scanner input = new Scanner(System.in);
+    	while (true) {
+    		System.out.println("Order Management System");
+        	System.out.println("Enter number for function:");
+        	System.out.println("1. Add order");
+        	System.out.println("2. Edit order");
+        	System.out.println("3. Remove order");
+        	System.out.print("->");
+        	
+        	int userInput = input.nextInt();
+    		if (userInput == 1) {
+    			System.out.println("Add new order: ");
+    			break;
+    		} else if (userInput == 2) {
+    			System.out.println(Orders.printOrders());
+    			System.out.println("Edit order: ");
+    			int editInput = input.nextInt();
+    			Orders.getOrderInfo(editInput);
+    			break;
+    		} else if (userInput == 3) {
+    			System.out.println("Remove order: ");
+    			break;
+    		} else {
+    			System.out.println("Wrong input!");
+    		}
+    	}
+    }
 }
---
+++ src/main/java/com/arcada/devops/dennis_project/Orders.java
@@ -0,0 +1,87 @@
+package com.arcada.devops.dennis_project;
+
+public class Orders {
+
+	public static String printOrders() {
+		String customer = 
+				"1. NovaTech Solutions  \r\n"
+				+ "2. GreenLeaf Innovations  \r\n"
+				+ "3. StellarWave Media  \r\n"
+				+ "4. Quantum Nest Designs  \r\n"
+				+ "5. UrbanHive Marketing  \r\n"
+				+ "6. BrightSky Ventures  \r\n"
+				+ "7. EcoSphere Industries  \r\n"
+				+ "8. Apex Horizon Consulting  \r\n"
+				+ "9. Luminous Path Technologies  \r\n"
+				+ "10. Driftwood Creations"; 
+		
+		return customer;
+	}
+
+	public static void getOrderInfo(int editInput) {
+		if (editInput == 1) {
+			System.out.println("NovaTech Solutions\r\n"
+					+ "Price: 100 000€\r\n"
+					+ "Amount: 10 000\r\n"
+					+ "Item: Keyboard");
+		}
+		else if (editInput == 2) {
+			System.out.println("GreenLeaf Innovations\r\n"
+					+ "Price: 1 000 000€\r\n"
+					+ "Amount: 10 000\r\n"
+					+ "Item: Computer");
+		}
+		else if (editInput == 3) {
+			System.out.println("StellarWave Media\r\n"
+					+ "Price: 200 000€\r\n"
+					+ "Amount: 1 000\r\n"
+					+ "Item: Monitor");
+		}
+		else if (editInput == 4) {
+			System.out.println("Quantum Nest Designs\r\n"
+					+ "Price: 3 000 000€\r\n"
+					+ "Amount: 100 000\r\n"
+					+ "Item: Processor");
+		}
+		else if (editInput == 5) {
+			System.out.println("UrbanHive Marketing\r\n"
+					+ "Price: 50 000€\r\n"
+					+ "Amount: 1 000\r\n"
+					+ "Item: TV");
+		}
+		else if (editInput == 6) {
+			System.out.println("BrightSky Ventures\r\n"
+					+ "Price: 80 000€\r\n"
+					+ "Amount: 4 000\r\n"
+					+ "Item: Window");
+		}
+		else if (editInput == 7) {
+			System.out.println("EcoSphere Industries\r\n"
+					+ "Price: 100 000€\r\n"
+					+ "Amount: 100 000\r\n"
+					+ "Item: Planters");
+		}
+		else if (editInput == 8) {
+			System.out.println("Apex Horizon Consulting\r\n"
+					+ "Price: 50 000€\r\n"
+					+ "Amount: 100 000\r\n"
+					+ "Item: Pen");
+		}
+		else if (editInput == 9) {
+			System.out.println("Luminous Path Technologies\r\n"
+					+ "Price: 1 000 000€\r\n"
+					+ "Amount: 1 000\r\n"
+					+ "Item: Laptop");
+		}
+		else if (editInput == 10) {
+			System.out.println("Driftwood Creations\r\n"
+					+ "Price: 10 000€\r\n"
+					+ "Amount: 10 000\r\n"
+					+ "Item: Book");
+		}
+		else {
+			System.out.println("No order matching " + editInput);
+		}
+	}
+
+}
